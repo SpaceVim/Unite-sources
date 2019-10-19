2f5ad57043d922bbadcd4653ea69dab5ee09b15f
wsdjeg <wsdjeg@outlook.com>
2019-10-19 23:08:33 +0800

    Fix

--- autoload/unicode.vim
+++ autoload/unicode.vim
@@ -4,3 +4,11 @@ function! unicode#groups()
           \ '[fnamemodify(v:val, ":t:r"), fnamemodify(v:val, ":p")]')
 
 endfunction
+
+function! unicode#words(groups)
+
+    let unicode = []
+    call map(a:groups, 'extend(unicode, readfile(g:unite_unicode_data_path . v:val . ".txt"))')
+    return unicode
+
+endfunction
--- rplugin/python3/denite/source/unicode_select.py
+++ rplugin/python3/denite/source/unicode_select.py
@@ -7,15 +7,14 @@ class Source(Base):
     def __init__(self, vim):
         Base.__init__(self, vim)
 
-        self.name = 'unicode'
+        self.name = 'unicode_select'
         self.kind = 'word'
 
     def gather_candidates(self, context):
         candidates = []
-        for [ugroup, path] in self.vim.call("unicode#groups"):
+        for line in self.vim.call("unicode#words", context.args):
             candidates.append({
-                'kind': 'unicode',
-                'word': ugroup
+                'word': line
                 })
         return candidates
 
